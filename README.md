355fab71d06c3e734ea67033f6faf7d2a760f94c
Blair Learn <blairl-nih@users.noreply.github.com>
2019-09-13 17:06:57 -0400

    (#3) Perform domain assignment. - Define SFRequest.post() - Define SFDomains.add() to perform assignment
    Need to allow caller to decide on Content-Type (or other headers)
    rather than hard-coding in SFRequest.post().

--- README.md
+++ README.md
@@ -9,4 +9,28 @@ We started by trying to containerize Drush commands, but found that you pretty m
 
 ## Tools
 
-### Insert tools here.
+### Domain Mapping
+
+Sets up the domain mappings for an ACSF lower tier.
+
+_This is an ongoing work-in-progress._
+
+#### Get an ACSF API Key
+1. Log in to the production Site Factory site.
+2. In the upper-right corner of the page, click on your email address.
+3. Click on the "API key" tab.
+4. Copy your API key to a _safe_ location. This key allows anyone who has it to take actions as _you_.
+
+#### Setup.
+1. CLone this repository.
+2. run `npm install`
+3. In `<REPO_BASE>/config`, copy `default.json` to `local.json`.  This is where _your_ individual settings go.
+4. Replace the following items:
+   * **username:** The email address associated with your ACSF account.
+   * **apikey:** Your API key (See above)
+   * **factoryHost:** The fully-qualified DEV or TEST Site Factory site name.
+5. From the Box "Sensitive Files" directory, copy either `domainlist.json.dev` or `domainlist.json.test` and place it in `<REPO_BASE>` as `domainlist.json`.  This file lists the domains associated with each of the hostnames on a given tier.
+
+#### Run the script
+
+From `<REPO_ROOT>`, execute the command `node index.js`.
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const config            = require('config');
-const SiteFactoryClient = require('./lib/site-factory-client')
+const config = require('config');
+const SiteFactoryClient = require('./lib/site-factory-client');
+const fs = require('fs');
 
 const factoryConn = config.get('factoryConnection');
 
@@ -9,15 +10,69 @@ const client = new SiteFactoryClient({
   factoryHost: factoryConn.factoryHost
 });
 
+
 async function main() {
   try {
-    const response = await client.sites.list();
 
-    //console.log(JSON.stringify(response));
-    console.log(response);
+    const [sitelist, domainlist] = await Promise.all([
+      client.sites.list(), // List of ACSF sites
+      getDomains()         // List of domains
+    ]);
+
+    doAssignments(sitelist, domainlist);
+
   } catch (err) {
     console.error(err);
   }
+}
+
+async function doAssignments(sitelist, domainlist) {
+
+  sitePromises = [];
+  sitelist.forEach(site => {
+    sitePromises += doPerSiteAssignments(site.id, domainlist[site.site]);
+  });
+  await Promise.all(sitePromises);
+}
+
+async function doPerSiteAssignments(siteId, domainInfo){
+
+  // Make sure the default domain is assinged first.
+  await client.domains.add(siteId, domainInfo.default);
+
+  // Set the additonal domains
+  additionalSites = [];
+  domainInfo.additional.forEach(domain => {
+    additionalSites += client.domains.add(siteId, domain);
+  });
+
+  await Promise.all(additionalSites);
+}
+
+/**
+ * Retrieve a list of the domains which are mapped to each
+ * of the ACSF sites.
+ *
+ * {
+ *   "host": {
+ *     "default": "default.cancer.gov",
+ *     "additional": [
+ *       "name1.cancer.gov",
+ *       "name2.cancer.gov"
+ *     ]
+ *   }
+ * }
+ *
+ */
+async function getDomains() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('domainlist.json', function read(err, data) {
+      if (err)
+        reject(err);
+      else
+        resolve(JSON.parse(data));
+    });
+  });
 }
 
 main();
